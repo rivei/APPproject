7aecf0158088b9088ec26c8ce292cf8486c93b49
AvaGhaf <ava.ghafari72@gmail.com>
2018-02-11 16:02:42 +0100

    drawer all working
    profile and report done with random data for now

--- WeiAva/app/src/main/java/it/polimi/two/weiava/activities/DocProfActivity.java
+++ WeiAva/app/src/main/java/it/polimi/two/weiava/activities/DocProfActivity.java
@@ -1,5 +1,6 @@
 package it.polimi.two.weiava.activities;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,17 +18,21 @@ public class DocProfActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_prof);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
         fab = (FloatingActionButton) findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(DocProfActivity.this,EmailActivity.class);
+                startActivity(intent);
+
             }
         });
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
 
